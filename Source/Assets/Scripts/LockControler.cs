ed6f0da636b3f9a80a04f3cbb714a89a2fcc3355
Behdad <bakhshi.behdad@gmail.com>
2015-11-13 20:06:15 -0700

    lock created

---
+++ Source/Assets/Scripts/LockControler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockControler : MonoBehaviour {
+
+	public bool state;
+	public Sprite lockSprite;
+	public Sprite unlockSprite;
+	private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		state = false;
+		sr = gameObject.GetComponent<SpriteRenderer>();
+		sr.sprite = lockSprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+		GameObject otherGameObject = other.gameObject;		
+		if (otherGameObject.CompareTag ("Ball")) {
+			if(!state){
+				state = true;
+				sr.sprite = unlockSprite;
+			}
+			else{
+				state = false;
+				sr.sprite = lockSprite;
+			}
+		} 
+	}
+}
