b6eff6bcd8b7fa62401bf202b14c532682b81335
Behdad <bakhshi.behdad@gmail.com>
2015-11-15 17:22:16 -0700

    portal initiation

---
+++ Source/Assets/Scripts/PortalControler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalControler : MonoBehaviour {
+
+	public bool state;
+	public float timeout;
+
+	// Use this for initialization
+	void Start () {
+		state = false;
+		timeout = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeout -= Time.deltaTime;
+		timeout = Mathf.Max(timeout, 0);
+		if (timeout == 0) {
+			state = false;
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		GameObject otherGameObject = other.gameObject;		
+		if (otherGameObject.CompareTag ("Ball")) {
+			if(timeout == 0){
+				state = true;
+				timeout = 1;
+			}
+		} 
+	}
+}
---
+++ Source/Assets/Scripts/PortalManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalManager : MonoBehaviour {
+
+	public GameObject ball;
+
+	private PortalControler [] portals;
+
+	// Use this for initialization
+	void Start () {
+		portals = this.GetComponentsInChildren<PortalControler> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool isit = false;
+		PortalControler p = null;
+
+		foreach (PortalControler portal in portals) {
+			if(portal.state){
+				isit = true;
+				portal.state = false;
+				p = portal;
+			}
+		}
+		int jump = Random.Range (0, portals.Length-1);
+
+		if (isit) {
+			foreach (PortalControler portal in portals) {
+				if(portal != p){
+					if(jump == 0)
+						ball.transform.position = portal.transform.position;
+
+					jump--;
+				}
+				portal.timeout = 1;
+				portal.state = false;
+			}
+		}
+	}
+}
