93927faac349419e414ccfa2c66a8c8f70ed3679
Behdad <bakhshi.behdad@gmail.com>
2015-11-27 19:27:58 -0700

    new obstacle (scene8)

---
+++ Source/Assets/Scripts/Rotater.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotater : MonoBehaviour {
+
+	public float rotatingSpeed;
+	public bool clockwise;
+	public Vector3 center;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.RotateAround (center, new Vector3(0,0,clockwise?1:-1), rotatingSpeed * Time.deltaTime );
+	}
+}
