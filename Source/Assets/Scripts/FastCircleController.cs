dcd6094a6eea9660787b306638df46c1481eff35
LOG <m.einakchi@gmail.com>
2015-11-30 21:14:43 -0500

    added scene9

---
+++ Source/Assets/Scripts/FastCircleController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FastCircleController : MonoBehaviour {
+
+	public float fastnessCoefficient = 2f;	
+	
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag == "Ball")
+		{
+			other.gameObject.GetComponent<Rigidbody2D>().velocity *= fastnessCoefficient;
+		}
+	}
+	
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.gameObject.tag == "Ball")
+		{
+			other.gameObject.GetComponent<Rigidbody2D>().velocity /= fastnessCoefficient;
+		}
+	}
+}
